9127e2de2b4895d6895dcf570814d5cd49f42e3c
JunseoYoun <junsueg5737@gmail.com>
2016-10-03 10:35:17 +0900

    * First Commit
    - 급식
    - 공지
    - 채팅

---
+++ app/src/main/java/net/plzpoint/kgshmaster/Fragment/HomeFragment.kt
@@ -0,0 +1,13 @@
+package net.plzpoint.kgshmaster.Fragment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import net.plzpoint.kgshmaster.R
+
+class HomeFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_fragment)
+    }
+}
---
+++ app/src/main/java/net/plzpoint/kgshmaster/other/CircleTransform.kt
@@ -0,0 +1,10 @@
+package net.plzpoint.kgshmaster.other
+
+/**
+ * Created by junsu on 2016-10-03.
+ */
+
+open class CircleTransform()
+{
+
+}
