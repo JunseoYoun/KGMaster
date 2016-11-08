8d8bb9dec199b4a6797d123b919b4068d8265ff9
JunseoYoun <junsueg5737@gmail.com>
2016-11-09 01:30:55 +0900

    * Added
    + KG Meal Contents Base
    + KG Profile ( image, nick, comment ) Change
    
    + Galaxy7 Resolution Error

--- app/src/main/java/net/plzpoint/kgmaster/MainActivity.kt
+++ app/src/main/java/net/plzpoint/kgmaster/MainActivity.kt
@@ -3,11 +3,16 @@ package net.plzpoint.kgmaster
 import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.support.annotation.IdRes
 import android.support.design.widget.NavigationView
 import android.support.v4.app.ActivityCompat
@@ -17,18 +22,21 @@ import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.pawegio.kandroid.find
 import net.plzpoint.kgmaster.Fragment.ChatFragment
 import net.plzpoint.kgmaster.Fragment.MealFragment
 import net.plzpoint.kgmaster.Fragment.NewsFragment
 import net.plzpoint.kgmaster.Fragment.NotifiFragment
 import net.plzpoint.kgmaster.Util.CircleTransform
 import java.io.BufferedOutputStream
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 
@@ -45,6 +53,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     var navigationHead: View? = null
 
     var profileImage: ImageView? = null
+    var profileNick: TextView? = null
+    var profileComment: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -69,13 +79,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         kgSettingNotification!!.setOnCheckedChangeListener { compoundButton, b -> }
 
         // ----------------------------------- Profile Image
-        profileImage = navigationHead?.findViewById(R.id.profile) as ImageView
+        profileImage = navigationHead?.findViewById(R.id.kg_profile_image) as ImageView
         profileImage?.setImageResource(R.mipmap.bell)
         profileImage?.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 imageSelection()
             }
         })
+        // ----------------------------------- Profile ( Nick, Comment )
+        profileNick = navigationHead?.findViewById(R.id.kg_profile_nick) as TextView
+        profileComment = navigationHead?.findViewById(R.id.kg_profile_comment) as TextView
+
         replaceContentFragment(R.id.content_main, fragMeal)
     }
 
@@ -94,15 +108,17 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         if (id == R.id.kg_meal) {
             replaceContentFragment(R.id.content_main, fragMeal)
+            onBackPressed()
         } else if (id == R.id.kg_talk) {
             replaceContentFragment(R.id.content_main, fragChat)
+            onBackPressed()
         } else if (id == R.id.kg_notification) {
             replaceContentFragment(R.id.content_main, fragNotifi)
+            onBackPressed()
         } else if (id == R.id.kg_news) {
             replaceContentFragment(R.id.content_main, fragNews)
+            onBackPressed()
         }
-        val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
-        drawer.closeDrawer(GravityCompat.START)
         return true
     }
 
@@ -113,28 +129,40 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     // ----------- ImageSelection
     val SELECT_PICTURE = 0
-    val SELECT_CAMERA = 1
-
-    val MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1
+    val CROP_IMAGE = 1
+    var tempImageUri: Uri? = null
 
     fun imageSelection() {
         val cameraListener = object : DialogInterface.OnClickListener {
             override fun onClick(dialog: DialogInterface?, which: Int) {
-
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI.toString())
+                // 이미지 잘라내기 위한 크기
+                intent.putExtra("crop", "true")
+                intent.putExtra("aspectX", 0)
+                intent.putExtra("aspectY", 0)
+                intent.putExtra("outputX", 200)
+                intent.putExtra("outputY", 150)
+                intent.putExtra("output", tempImageUri)
+                try {
+                    //intent.putExtra("return-data", true)
+                    startActivityForResult(intent, CROP_IMAGE)
+                } catch (e: ActivityNotFoundException) {
+                    // Do nothing for now
+                }
             }
         }
         val albumListener = object : DialogInterface.OnClickListener {
             override fun onClick(dialog: DialogInterface?, which: Int) {
-                //val intent = Intent()
-                //intent.type = "image/*"
-                //intent.action = Intent.ACTION_GET_CONTENT
-                //startActivityForResult(Intent.createChooser(intent, "이미지 선택"), SELECT_PICTURE)
-                val intent = Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                intent.type = "image/*"
-                startActivityForResult(Intent.createChooser(intent, "이미지 선택"), SELECT_PICTURE)
+                val intent = Intent(Intent.ACTION_PICK);
+                intent.type = android.provider.MediaStore.Images.Media.CONTENT_TYPE
+                try {
+                    startActivityForResult(Intent.createChooser(intent, "이미지 선택"), SELECT_PICTURE)
+                } catch (e: ActivityNotFoundException) {
+                }
             }
         }
-
         val cancelListener = object : DialogInterface.OnClickListener {
             override fun onClick(dialog: DialogInterface?, which: Int) {
                 dialog!!.dismiss()
@@ -152,27 +180,45 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         if (resultCode == RESULT_OK) {
             when (requestCode) {
                 SELECT_PICTURE -> {
-                    val selectedImageUri = data!!.data
+                    tempImageUri = data!!.data
 
-                    Glide.with(this)
-                            .load(selectedImageUri)
-                            .crossFade()
-                            .thumbnail(0.5f)
-                            .bitmapTransform(CircleTransform(this))
-                            .diskCacheStrategy(DiskCacheStrategy.ALL)
-                            .into(profileImage)
+                    val intent = Intent("com.android.camera.action.CROP")
+                    intent.setDataAndType(tempImageUri, "image/*")
+                    intent.putExtra("outputX", 90)
+                    intent.putExtra("outputY", 90)
+                    intent.putExtra("aspectX", 1)
+                    intent.putExtra("aspectY", 1)
+                    intent.putExtra("scale", true)
+                    intent.putExtra("return-data", true)
+                    startActivityForResult(intent, CROP_IMAGE)
                 }
-                SELECT_CAMERA -> {
 
+                CROP_IMAGE -> {
+                    val extras = data!!.extras
+                    if (extras != null) {
+                        val photo: Bitmap = extras.getParcelable("data")
+                        profileImage!!.setImageBitmap(photo)
+                    }
+                    val f = File(tempImageUri!!.getPath())
+                    if (f.exists()) {
+                        f.delete()
+                    }
+
+                    //Glide.with(this)
+                    //        .load(filePath)
+                    //        .crossFade()
+                    //        .thumbnail(0.5f)
+                    //        .bitmapTransform(CircleTransform(this))
+                    //        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    //        .into(profileImage)
                 }
             }
         }
     }
 
-    fun storeImage(bitmap: Bitmap, filePath: String) {
+    private fun storeImage(bitmap: Bitmap, filePath: String) {
         val copyFile = File(filePath)
-        var out: BufferedOutputStream? = null
-
+        var out: BufferedOutputStream?
         try {
             copyFile.createNewFile()
             out = BufferedOutputStream(FileOutputStream(copyFile))
@@ -182,5 +228,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
+
+    fun getImageUri(inContext: Context, inImage: Bitmap): Uri {
+        val bytes = ByteArrayOutputStream()
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+        val path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null)
+        return Uri.parse(path)
     }
 }
